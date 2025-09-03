8a25158234b797ed65ae4eb5846b864c2e0bed34
obabadi76 <obabadi76@gmail.com>
2025-09-03 06:36:05 +0000

    just saving

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:myapp/core/common/application/cache_helper.dart';
-import 'package:myapp/core/services/injection_container.dart';
 import 'package:myapp/src/crypto/presention/view/crypto_view.dart';
 import 'package:myapp/src/dashboard/presention/views/dashboard_screen.dart';
 import 'package:myapp/src/currency/presention/views/curreny_view.dart';
 import 'package:myapp/src/gold/presention/views/golds_view.dart';
-import 'package:myapp/src/on_boarding/presention/viewis/on_boarding_screen.dart';
 import 'package:myapp/src/splash_screen/presention/views/splash_screen_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../src/error/presention/views/error_screen.dart';
 
